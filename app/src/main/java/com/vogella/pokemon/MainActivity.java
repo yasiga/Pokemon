e196c775077c4489218fa95337697104b630ddf7
yasiga <yasiga@hotmail.fr>
2020-05-15 11:51:43 +0200

    Initial commit

---
+++ app/src/main/java/com/vogella/pokemon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vogella.pokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
